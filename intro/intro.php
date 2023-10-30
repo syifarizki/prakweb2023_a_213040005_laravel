51ee82b99158e7f4e4d349219ab2d8ed6d12ae39
syifarizki <rizkisyifa379@gmail.com>
2023-10-30 08:57:28 +0700

    Video 1

---
+++ intro/intro.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+
+Web application framework
+    - Sebuah 'kerangka yang di desain untuk mendukung pembangunan aplikasi web 
+    - Menyediakan standar/cara untuk membangun aplikasi web
+    - Bertujuan untuk mengotomasi hal-hal umum yang biasanya dilakukan saat membangun aplikasi web
+    - Contohnya : database library, templating engine, session management, authentication, security, dll
+
+
+LARAVEL
+"Laravel is a web application framework with expensive, elegant sytax."
+- Laravel dibuat oleh Taylor Otwell pada juni 2011 sebagai alternatif framework untuk CodeIgniter versi terbarunya versi 8
+- Filosofi Laravel : "We belive development mush be an enjoyable, creative experience to be truly fulfilling."
+- Tujuan Laravel : "Laravel  aims to make the development process a pleasing one for the developer without sacrificing application functionality. Happy developer make the best code."
+
+
+Fitur utama pada laravel :
+    - MVC
+    - Templating engine
+    - Artisan console
+    - Eloquent ORM
+    - Authentication & authorization
+    - Testing
+    - Packaging system
+    - Multiple file system
+    - Task
+    - Scheduling
+    - Websocker programming
+
+
+Requirement :
+    - PHP 7.4/8
+    - MySQL, Composer 2
+    - SequelPRO / MySQL workbench
+    - Terminal/CMD/Powershell/GitBash
+    - Code editor : Visual studio code
+    - Extentions : PHP intelephense, laravel artisan, laravel snippets, laravel blade snippets, laravel blade spacer, laravel GoTo view
+    
+*/
