afe62c933372a435c8261fb023295a88ff4e5af5
syifarizki <rizkisyifa379@gmail.com>
2023-11-06 20:01:40 +0700

    Video 13

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -3,11 +3,11 @@
 @section('container')
 
 <div class="container">
-    <div class="row justify-content-center mb-5">
+    <div class="row justify-content-center">
         <div class="col-md-8">
             <h1 class="mb-3">{{ $post->title }}</h1>
 
-            <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">
+            <p>By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">
             {{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" 
             class="text-decoration-none">{{ $post->category->name }}</a></p>
 
