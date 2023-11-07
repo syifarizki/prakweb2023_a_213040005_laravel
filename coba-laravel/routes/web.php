1d8483ddfaacf6359622f49003ae83fafb3f8121
syifarizki <rizkisyifa379@gmail.com>
2023-11-07 10:22:26 +0700

    Video 15

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -57,4 +57,5 @@ Route::get('/categories', function() {
 Route::get('/login', [LoginController::class, 'index']);
 
 Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
 
